2aadd38704b247c5a9a632285b60f1cdb5be2b96
Your Name <you@example.com>
2023-06-13 23:08:44 -0500

    Pantalla inicio sesion

--- app/src/main/java/com/pjimenez/cakeandbake/MainActivity.java
+++ app/src/main/java/com/pjimenez/cakeandbake/MainActivity.java
@@ -2,13 +2,22 @@ package com.pjimenez.cakeandbake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button boton_iniciar_sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton_iniciar_sesion = findViewById(R.id.btn_iniciar_sesion);
+        boton_iniciar_sesion.setOnClickListener(v -> {
+            Intent objeto = new Intent(this, IniciarSesion.class);
+            startActivity(objeto);
+        });
     }
 }
