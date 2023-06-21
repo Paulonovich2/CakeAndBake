26da4545075cebc8d008623ef1e9b9be7f6670a3
Desarrollo Mx1 <Desarrollo.Mx1@talma.com.mx>
2023-06-21 17:53:07 -0500

    Producto pedido

---
+++ app/src/main/java/com/pjimenez/cakeandbake/ProductoPedido.java
@@ -0,0 +1,14 @@
+package com.pjimenez.cakeandbake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductoPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto_pedido);
+    }
+}
